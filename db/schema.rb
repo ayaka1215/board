c5c4e2a14435e44f02923ced1124fcd7102cedc0
AyakaOoshima <och1215bi3@gmail.com>
2022-10-30 08:39:09 +0900

    commentモデル・テーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_29_101328) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_29_233429) do
   create_table "boards", charset: "utf8mb4", force: :cascade do |t|
     t.string "name"
     t.string "title"
@@ -19,4 +19,14 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_29_101328) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "comments", charset: "utf8mb4", force: :cascade do |t|
+    t.bigint "board_id", null: false
+    t.string "name", null: false
+    t.text "comment", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["board_id"], name: "index_comments_on_board_id"
+  end
+
+  add_foreign_key "comments", "boards"
 end
