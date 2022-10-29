831ba08f0c318bd5f0938dba23711cd6c8cb061d
AyakaOoshima <och1215bi3@gmail.com>
2022-10-29 20:17:45 +0900

    掲示板一覧　修正

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,8 @@ module BoardProject
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # 日本時間設定
+    config.time_zone = 'Tokyo'
   end
 end
