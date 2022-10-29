f8b5e4a90ef6ec63383963ebd1ddba5ddf286308
AyakaOoshima <och1215bi3@gmail.com>
2022-10-30 08:08:32 +0900

    掲示板バリデーション設定（msgを日本語にできていない）

--- config/application.rb
+++ config/application.rb
@@ -21,5 +21,9 @@ module BoardProject
 
     # 日本時間設定
     config.time_zone = 'Tokyo'
+
+    # 日本語設定
+    config.i18n.default_locate = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
   end
 end
