6f6a7249946e6d9a012c9189bd6808b367efb18a
AyakaOoshima <och1215bi3@gmail.com>
2022-10-31 12:47:54 +0900

    ヘッダーメニュー作成

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+    def header_link_item(name, path)
+        class_name = 'nav_item'
+        # 今のパスと引数のパスが同一か
+        class_name << ' avtive' if current_page?(path)
+
+        content_tag :li, class: class_name do
+            link_to name, path, class: 'nav-link'
+        end 
+    end
 end
